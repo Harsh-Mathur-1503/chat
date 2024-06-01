0dac64d447ba1af12e2427a8c284e8ca4dd5557e
Harsh Mathur <harshmathur@Harshs-MacBook-Air.local>
2024-06-01 11:33:08 +0530

    folder and file setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter ,Routes,Route } from "react-router-dom"
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/services" element={<h1>Services</h1>} />
+      <Route path="/contact" element={<h1>Contact</h1>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
